31f6bb49ea575e5db0f443a60a45d15a8a225d44
Priya maurya <priyamauryamzp978@gmail.com>
2024-10-26 18:17:41 +0530

    complete signUp page functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDB!');
@@ -18,7 +19,9 @@ app.listen(3000,()=>{
 );
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+
 app.use((err,req,res,next)=>{
+    console.error('Server Error:',err);
 const statusCode=err.statusCode || 500;
 const message=err.message || 'Internal Server Error';
 return res.status(statusCode).json({
